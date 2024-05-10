f616739f56e2c2f96a2f278cbe64d96213e76194
Abhay Tiwari <abhayskp7948@gmail.com>
2024-05-10 15:48:51 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const AdminRouter = require('./Routers/Admin');
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173','https://bbdu-backend-2.onrender.com','https://bbducreatorfrontend.vercel.app/'],
+    origin: ['http://localhost:5173','https://bbdu-backend-2.onrender.com','https://finalfrontend-sigma.vercel.app'],
     credentials: true
 }))
 app.use(express.json());
